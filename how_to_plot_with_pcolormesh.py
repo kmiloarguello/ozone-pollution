719acb07efb8c2f7c4c7eed31d80376e0d7fd1d2
CA <camilo.arguello.rincon@gmail.com>
2021-03-13 11:45:32 +0100

    [CA] Update

---
+++ how_to_plot_with_pcolormesh.py
@@ -0,0 +1,17 @@
+nrows = 3
+ncols = 5
+Z = np.arange(nrows * ncols).reshape(nrows, ncols)
+x = np.arange(ncols + 1)
+y = np.arange(nrows + 1)
+
+fig, ax = plt.subplots()
+ax.pcolormesh(x, y, Z, shading='flat', cmap="jet",edgecolors='k', linewidths=1, vmin=Z.min(), vmax=Z.max())
+
+
+def _annotate(ax, x, y, title):
+    # this all gets repeated below:
+    X, Y = np.meshgrid(x, y)
+    ax.plot(X.flat, Y.flat, 'o', color='m')
+    ax.set_title(title)
+
+_annotate(ax, x, y, "shading='flat'")
